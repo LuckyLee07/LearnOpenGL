3dd8a5ebfc072e4c85306724591035f83490073f
FancyLee07 <071427li@163.com>
2022-03-23 23:01:20 +0800

    创建并使用shader、针对MacOS的一些兼容问题

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,54 @@
+#include <string>
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+static unsigned int CompilerShader(unsigned int type, const std::string& source)
+{
+    unsigned int shaderId = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(shaderId, 1, &src, nullptr);
+    glCompileShader(shaderId);
+
+    GLint result;
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &result);
+    if (result != GL_TRUE)
+    {
+        GLint length;
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &result);
+        //char* message = (char*)alloca(length * sizeof(char));
+        //second args: maxLength
+        //glGetShaderInfoLog(shaderId, length, &length, message);
+        char message[1024];
+        glGetShaderInfoLog(shaderId, 1024, &length, message);
+
+        const char* sType = type == GL_VERTEX_SHADER ? "vertex" : "fragment";
+        std::cout << "Failed to compile " << sType << "shader!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(shaderId);
+        return 0;
+    }
+    return shaderId;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int programId = glCreateProgram();
+    unsigned int vs = CompilerShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompilerShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(programId, vs);
+    glAttachShader(programId, fs);
+    glLinkProgram(programId);
+    glValidateProgram(programId);
+
+    //glDetachShader/glDeleteShader：Detach后无源码调试麻烦
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return programId;
+}
 
 int main(void)
 {
@@ -11,6 +59,14 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    std::cout << "I'm aple machine" << std::endl;
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -18,6 +74,10 @@ int main(void)
         glfwTerminate();
         return -1;
     }
+
+    unsigned int major = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MAJOR);
+    unsigned int minor = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MINOR);
+    std::cout << "oepngl shader version: " << major << "." << minor << std::endl;
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -43,6 +103,28 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, false, 2 * sizeof(float), (const void *)0);
 
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "\n}";
+    std::string fragmentShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 1.0, 0.0, 1.0);\n"
+        "\n}";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
